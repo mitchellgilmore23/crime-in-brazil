125a04649b76bb4da6d57d46313bbe221effee6b
mitchellgilmore23 <117942362+mitchellgilmore23@users.noreply.github.com>
2023-04-06 13:46:06 -0500

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const file = 'Violence In Brazil.js';
+module.exports = {
+	entry: {
+		index: './src/index.js',
+		page1: './src/Violence In Brazil.js',
+	},
+	output: {
+		filename: '[name].js',
+	},
+};

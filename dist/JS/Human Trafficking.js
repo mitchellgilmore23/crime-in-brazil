14f965bcbe8cd15ca84b7a37ea0f4e4282a05f9b
Kenobi127 <mateolm1207@gmail.com>
2023-04-18 00:57:00 -0500

    started second page

--- dist/JS/Human Trafficking.js
+++ dist/JS/Human Trafficking.js
@@ -1,11 +1,3 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 var slideIndex = 1;
 var slides = document.querySelectorAll('.slide');
 var dots = document.querySelectorAll('.dot');
@@ -63,6 +55,14 @@ dot.addEventListener('click', function() {
   });
 });
 
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
